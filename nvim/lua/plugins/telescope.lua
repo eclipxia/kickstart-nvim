fb00c5c1012bc225a10466024035f2068142edb0
eclipxia <xaylei@outlook.com>
2025-08-06 16:11:26 -0400

    fixed keybinds

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -48,6 +48,12 @@ return {
       }
     })
 
+	vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+	vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live Grep' })
+	vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
+	vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help Tags' })
+	vim.keymap.set("n", "<leader>r", "<cmd>Telescope registers<CR>", { desc = "Telescope Registers" })
+	vim.keymap.set('n', '<leader>fc', builtin.git_commits, { desc = 'Find files' })
     -- Optional: Define keymaps for common Telescope pickers
   end,
 }
